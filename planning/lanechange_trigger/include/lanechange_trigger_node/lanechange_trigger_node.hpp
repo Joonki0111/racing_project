f9969731b0858656a4347853d42a0e1a595366ae
Joonki0111 <hjk3889@chosun.kr>
2024-09-02 11:07:19 +0900

    [ADD] request lanechange when curvature is below threshold

--- planning/lanechange_trigger/include/lanechange_trigger_node/lanechange_trigger_node.hpp
+++ planning/lanechange_trigger/include/lanechange_trigger_node/lanechange_trigger_node.hpp
@@ -8,6 +8,10 @@
 #include "example_interfaces/srv/trigger.hpp"
 #include "perception_msgs/msg/valid_path.hpp"
 #include "planning_msgs/msg/lanechange_status.hpp"
+#include "autoware_auto_planning_msgs/msg/path.hpp"
+#include "geometry_msgs/msg/point.hpp"
+#include <visualization_msgs/msg/marker.hpp>
+#include <visualization_msgs/msg/marker_array.hpp>
 
 #include <algorithm>
 
@@ -19,6 +23,8 @@ using tier4_rtc_msgs::msg::Module;
 using tier4_rtc_msgs::msg::Command;
 using perception_msgs::msg::ValidPath;
 using planning_msgs::msg::LanechangeStatus;  
+using autoware_auto_planning_msgs::msg::Path;  
+using geometry_msgs::msg::Point;  
 
 class LanechangeTrigger : public rclcpp::Node{
     public:
@@ -28,8 +34,10 @@ class LanechangeTrigger : public rclcpp::Node{
         rclcpp::Subscription<ValidPath>::SharedPtr sub_valid_path_;
         rclcpp::Subscription<CooperateStatusArray>::SharedPtr sub_rtc_status_;
         rclcpp::Subscription<LanechangeStatus>::SharedPtr sub_lanechange_status_;
+        rclcpp::Subscription<Path>::SharedPtr sub_main_path_;
 
         rclcpp::Publisher<CooperateStatusArray>::SharedPtr pub_rtc_status_;
+        rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr marker_pub_;
 
         rclcpp::Client<example_interfaces::srv::Trigger>::SharedPtr cli_trigger_;
 
@@ -38,14 +46,18 @@ class LanechangeTrigger : public rclcpp::Node{
         ValidPath::SharedPtr valid_path_msg_ptr_;
         CooperateStatusArray::SharedPtr rtc_status_msg_ptr_;
         LanechangeStatus::SharedPtr lanechange_status_msg_ptr_;
+        Path::SharedPtr main_path_msg_ptr_;
 
         void validpathCallback(const ValidPath::SharedPtr valid_path_msg);
         void rtcStatusCallback(const CooperateStatusArray::SharedPtr rtc_status_msg);
         void lanechangestatusCallback(const LanechangeStatus::SharedPtr lanechange_status_msg);
+        void mainpathCallback(const Path::SharedPtr main_path_msg);
         void run();
         bool checkSubscription();
         CooperateStatusArray createStatusesWithDirection(CooperateStatusArray & statuses_vec, const ValidPath & valid_path);
+        float calcCurvature(const Path & main_path_msg);
         void requestLanechange(const LanechangeStatus & lanechange_status_msg, const CooperateStatusArray & statuses_msg);
+        void pubMarker(const std::vector<Point> points_vec);
 };
 }
 #endif  //LANECHANGE_TRIGGER_NODE__LANECHANGE_TRIGGER_NODE_HPP_
--- planning/lanechange_trigger/src/lanechange_trigger_node.cpp
+++ planning/lanechange_trigger/src/lanechange_trigger_node.cpp
@@ -13,8 +13,11 @@ LanechangeTrigger::LanechangeTrigger(const rclcpp::NodeOptions & node_options)
     sub_lanechange_status_ = this->create_subscription<LanechangeStatus>(
         "/racing/lanechange_status", rclcpp::QoS(1),
             std::bind(&LanechangeTrigger::lanechangestatusCallback, this, std::placeholders::_1));
+    sub_main_path_ = this->create_subscription<Path>("/planning/scenario_planning/lane_driving/behavior_planning/path", 
+        rclcpp::QoS(1), std::bind(&LanechangeTrigger::mainpathCallback, this, std::placeholders::_1));
 
     pub_rtc_status_ = this->create_publisher<CooperateStatusArray>("/api/external/get/rtc_status", rclcpp::QoS(1));
+    marker_pub_ = this->create_publisher<visualization_msgs::msg::MarkerArray>("/HYD_racing/perception/objects_with_boundary/debug_marker", rclcpp::QoS(1)); //debug
 
     cli_trigger_ = this->create_client<example_interfaces::srv::Trigger>("/racing/lc_trigger");
     timer_ = this->create_wall_timer(std::chrono::milliseconds(100), std::bind(&LanechangeTrigger::run, this));
@@ -22,6 +25,7 @@ LanechangeTrigger::LanechangeTrigger(const rclcpp::NodeOptions & node_options)
     valid_path_msg_ptr_ = nullptr;
     rtc_status_msg_ptr_ = nullptr;
     lanechange_status_msg_ptr_ = nullptr;
+    main_path_msg_ptr_ = nullptr;
 }
 
 void LanechangeTrigger::validpathCallback(const ValidPath::SharedPtr valid_path_msg)
@@ -39,6 +43,11 @@ void LanechangeTrigger::lanechangestatusCallback(const LanechangeStatus::SharedP
     lanechange_status_msg_ptr_ = lanechange_status_msg;
 }
 
+void LanechangeTrigger::mainpathCallback(const Path::SharedPtr main_path_msg)
+{
+    main_path_msg_ptr_ = main_path_msg;
+}
+
 void LanechangeTrigger::run()
 {
     if(!checkSubscription())
@@ -49,7 +58,11 @@ void LanechangeTrigger::run()
     CooperateStatusArray statuses_msg = createStatusesWithDirection(*rtc_status_msg_ptr_, *valid_path_msg_ptr_);
     pub_rtc_status_->publish(statuses_msg);
 
-    requestLanechange(*lanechange_status_msg_ptr_, statuses_msg);
+    const float curvature = calcCurvature(*main_path_msg_ptr_);
+    if(curvature < 0.01f)
+    {
+        requestLanechange(*lanechange_status_msg_ptr_, statuses_msg);
+    }
 }
 
 bool LanechangeTrigger::checkSubscription()
@@ -110,6 +123,29 @@ CooperateStatusArray LanechangeTrigger::createStatusesWithDirection(CooperateSta
     return filtered_statuses_vec;
 }
 
+float LanechangeTrigger::calcCurvature(const Path & main_path_msg)
+{
+    std::vector<Point> lane_bound_vec;
+
+    for(int i = 0; i < 15; i += 5) //PARAM
+    {
+        lane_bound_vec.push_back(main_path_msg.right_bound[i]);
+    }
+
+    const float area_of_triangle = std::fabs((lane_bound_vec[0].x * (lane_bound_vec[1].y - lane_bound_vec[2].y) + 
+        lane_bound_vec[1].x * (lane_bound_vec[2].y - lane_bound_vec[0].y) + 
+            lane_bound_vec[2].x * (lane_bound_vec[0].y - lane_bound_vec[1].y)) / 2);
+
+    const float d21 = std::sqrt(std::pow((lane_bound_vec[1].x - lane_bound_vec[0].x), 2) + std::pow((lane_bound_vec[1].y - lane_bound_vec[0].y), 2));
+    const float d32 = std::sqrt(std::pow((lane_bound_vec[2].x - lane_bound_vec[1].x), 2) + std::pow((lane_bound_vec[2].y - lane_bound_vec[1].y), 2));
+    const float d31 = std::sqrt(std::pow((lane_bound_vec[2].x - lane_bound_vec[0].x), 2) + std::pow((lane_bound_vec[2].y - lane_bound_vec[0].y), 2)); 
+
+    const float curvature = 4 * area_of_triangle / (d21 * d32 * d31);  
+
+    pubMarker(lane_bound_vec);
+    return curvature;
+}
+
 void LanechangeTrigger::requestLanechange(const LanechangeStatus & lanechange_status_msg, const CooperateStatusArray & statuses_msg)
 {
     example_interfaces::srv::Trigger::Request::SharedPtr request = std::make_shared<example_interfaces::srv::Trigger::Request>();
@@ -128,6 +164,35 @@ void LanechangeTrigger::requestLanechange(const LanechangeStatus & lanechange_st
     {
         cli_trigger_->async_send_request(request);
     }
+}
+
+void LanechangeTrigger::pubMarker(const std::vector<Point> points_vec)
+{
+  visualization_msgs::msg::MarkerArray marker_vec;
+  int id = 0;
+
+  for(int i = 0; i < points_vec.size(); i++)
+  {
+    visualization_msgs::msg::Marker marker;
+    marker.header.frame_id = "map";
+    marker.header.stamp = this->now();
+    marker.ns = "point";
+    marker.id = id++;
+    marker.type = visualization_msgs::msg::Marker::CUBE;
+    marker.action =  visualization_msgs::msg::Marker::ADD;
+    marker.pose.position.x = points_vec[i].x;
+    marker.pose.position.y = points_vec[i].y;
+    marker.pose.position.z = points_vec[i].z;
+    marker.scale.x = 0.5;
+    marker.scale.y = 0.5;
+    marker.scale.z = 0.5;
+    marker.color.a = 1.0;
+    marker.color.r = 1.0;
+    marker.color.g = 0.0;
+    marker.color.b = 0.0;
+    marker_vec.markers.push_back(marker);
+  }
+  marker_pub_->publish(marker_vec);
 }
 } // namespace planning_component
 #include <rclcpp_components/register_node_macro.hpp>
